d4f371647adedc47590b5da94cfb9c4112818937
matheusmanoelberto <matheusmanoel51@gmail.com>
2024-12-18 19:27:46 -0300

    exception-filter

--- src/CashFlow.Api/Controllers/ExpensesController.cs
+++ src/CashFlow.Api/Controllers/ExpensesController.cs
@@ -1,7 +1,5 @@
 ﻿using CashFlow.Application.UserCases.Expenses.Register;
 using CashFlow.Comunnication.Requests;
-using CashFlow.Comunnication.Responses;
-using CashFlow.Exception.ExceptionsBase;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CashFlow.Api.Controllers;
@@ -13,23 +11,8 @@ public class ExpensesController : ControllerBase
     [HttpPost]
     public IActionResult Register([FromBody] RequestRegisterExpenseJson request)
     {
-        try
-        {
-            var userCase = new RegisterExpenseUseCase();
-            var response = userCase.Execute(request);
-            return Created(string.Empty, response);
-        }
-        catch (ErrorOnValidationException ex)
-        {
-            var errorResponse = new ResponseErrorJson(ex.Message);
-
-            return BadRequest(errorResponse);
-        }
-        catch
-        {
-            var errorResponse = new ResponseErrorJson("unknown error");
-
-            return StatusCode(StatusCodes.Status500InternalServerError, errorResponse);
-        }
+        var userCase = new RegisterExpenseUseCase();
+        var response = userCase.Execute(request);
+        return Created(string.Empty, response);
     }
 }
