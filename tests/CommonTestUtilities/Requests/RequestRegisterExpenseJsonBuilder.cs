1be2caf11fb9e08b177ee3d642349fe86547d6f1
matheusmanoelbert <matheusmanoel51@gmail.com>
2025-01-03 01:26:00 -0300

    test-validators

---
+++ tests/CommonTestUtilities/Requests/RequestRegisterExpenseJsonBuilder.cs
@@ -0,0 +1,20 @@
+﻿using Bogus;
+using CashFlow.Comunnication.Enums;
+using CashFlow.Comunnication.Requests;
+
+namespace CommonTestUtilities.Requests;
+
+public class RequestRegisterExpenseJsonBuilder
+{
+    public static RequestRegisterExpenseJson Build()
+    {
+
+       return new Faker<RequestRegisterExpenseJson>()
+            .RuleFor(r => r.Title, faker => faker.Commerce.ProductName())
+            .RuleFor(r => r.Description, faker => faker.Commerce.ProductDescription())
+            .RuleFor(r => r.Date, faker => faker.Date.Past())
+            .RuleFor(r => r.PaymentType, faker => faker.PickRandom<PaymentType>())
+            .RuleFor(r => r.Amount, faker => faker.Random.Decimal(min: 1, max: 1000));
+
+    }
+}
---
+++ tests/Validators.Test/Expenses/Register/RegisterExpenseValidatorTests.cs
@@ -0,0 +1,23 @@
+﻿using CashFlow.Application.UserCases.Expenses.Register;
+using CashFlow.Comunnication.Requests;
+using CommonTestUtilities.Requests;
+using FluentAssertions;
+
+namespace Validators.Test.Expenses.Register;
+
+public class RegisterExpenseValidatorTests
+{
+    [Fact]
+    public void Success()
+    {
+        //Arrange
+        var validator = new RegisterExpenseValidator();
+        var request = RequestRegisterExpenseJsonBuilder.Build();
+
+        //Act
+        var result = validator.Validate(request);
+
+        //Assert
+        result.IsValid.Should().BeTrue();
+    }
+}
